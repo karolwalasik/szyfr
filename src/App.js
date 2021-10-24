46273138d125f68edb7289924339b10701edf05d
Karol Walasik <k.walasik@andersenlab.com>
2021-10-24 18:31:09 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react'
+
+const moveLetter = (letter,key,encrypt=true) => {
+  let startCode = letter.charCodeAt(0);
+  if(letter === ' ') return ' '
+  if (startCode >= 97 && startCode <= 122) {
+    if(encrypt){
+      let result = (startCode +  Number(key))%97
+      if(result>26) result= result%26
+      return String.fromCharCode(result+97);
+    }else{
+      let result = (startCode  -Number(key))%97
+      if(result>26) {result= result+26}
+      else{
+        result= result%26+97
+      }
+      return String.fromCharCode(result);
+    }
+
+  } else if (startCode >= 65 && startCode <= 90) {
+    if(encrypt){
+      let result = (startCode +  Number(key))%65
+      if(result>26) result= result%26
+      return String.fromCharCode(result+65);
+    }else{
+      let result = (startCode  -Number(key))%65
+      if(result>26) {result= result+26}
+      else{
+        result= result%26+65
+      }
+      return String.fromCharCode(result);
+    }
+  } else return letter;
+};
+
+const caesar13 = (inputText,key,encrypt=true) => {
+  if (typeof inputText !== "string" || !inputText) {
+    throw new Error("not a string or empty string");
+  }
+  const arrayFromText = [...inputText];
+  const newArray = [];
+  arrayFromText.forEach(singleCharacter => {
+    let newLetter = moveLetter(singleCharacter,key,encrypt);
+    newArray.push(newLetter);
+  });
+  return newArray.join("");
+};
 
 function App() {
+  const [inputValue,setInputValue] = React.useState('')
+  const [keyInputValue,setKeyInputValue] = React.useState('')
+  const [decrypted,setDecrypted] = React.useState('')
+  const hanldeChangeInput = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  const hanldeChangeKeyInput = (e) => {
+    setKeyInputValue(e.target.value)
+  }
+
+  const handleEncrypt = () => {
+    if(inputValue && keyInputValue){
+      setDecrypted(caesar13(inputValue,keyInputValue,true))
+    }
+  }
+
+  const handleDecrypt = () => {
+    if(inputValue && keyInputValue){
+      setDecrypted(caesar13(inputValue,keyInputValue,false))
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={hanldeChangeInput}/>
+      key
+      <input onChange={hanldeChangeKeyInput}/>
+      <button onClick={handleEncrypt}>encrypt</button>
+      <button onClick={handleDecrypt}>decrypt</button>
+      {decrypted && <p>{decrypted}</p>}
     </div>
   );
 }
